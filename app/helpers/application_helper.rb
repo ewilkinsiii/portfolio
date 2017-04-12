4454c36f690aa784e39b4f4cfe8ece72c94e0b76
ewilkinsiii <ewilkinsiii@gmail.com>
2017-04-12 20:13:39 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    DarkstarudViewTool::Renderer.copyright "DarkStar UD", "All rights reserved"
+  end
 end
