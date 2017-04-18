ff2c0ef107e9e53edacbcdcfd21661af13d81a9b
ewilkinsiii <ewilkinsiii@gmail.com>
2017-04-18 05:24:17 +0000

    Added contact form, and about me content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,24 @@ class PagesController < ApplicationController
 
   def about
   end
-
+  
   def contact
   end
+  
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+      redirect_to contact_path
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  post 'contact', to: 'pages#create'
+  get 'contact/new', to: 'pages#new'
 
   resources :blogs do
     member do
