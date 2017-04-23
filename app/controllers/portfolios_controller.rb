097abba7b430c1cdf35a7ec648f6ca6d04421089
ewilkinsiii <ewilkinsiii@gmail.com>
2017-04-23 17:37:16 +0000

    Implemented persistent position updates for frage and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,6 +10,15 @@ class PortfoliosController < ApplicationController
    #@ruby_on_rails_portfolio_items = Portfolio.ruby_on_rails
   end
   
+  
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    
+    render nothing: true
+  end
+  
   def show
   end
   
