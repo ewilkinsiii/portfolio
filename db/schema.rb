0b85a2c67733643ef33f98b8ec3919382ec81c3a
ewilkinsiii <ewilkinsiii@gmail.com>
2017-05-10 20:25:42 +0000

    Implemented Category and added category to portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502164641) do
+ActiveRecord::Schema.define(version: 20170510193342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20170502164641) do
     t.string   "image"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
     t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -87,6 +93,8 @@ ActiveRecord::Schema.define(version: 20170502164641) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "position"
+    t.string   "client"
+    t.integer  "category_id"
   end
 
   create_table "skills", force: :cascade do |t|
